b015d66918aa60c4699f3240d33fad8a1f06b038
Wyatt VO <votranquy96@gmail.com>
2023-08-30 22:29:06 +0700

    Handle quizform

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  <quiz-form></quiz-form>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+// import HelloWorld from "./components/HelloWorld.vue";
+import QuizForm from "./components/QuizForm.vue";
+import "./assets/styles/global.css";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    // HelloWorld,
+    QuizForm,
   },
 };
 </script>
